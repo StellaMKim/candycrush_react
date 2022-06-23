d82e45ed64bbbdd707b828e0a42a2e20f8affad4
StellaMKim <popscene7171@gmail.com>
2022-06-22 20:26:39 -0700

    checking for matches completed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,41 @@ const candyColors = [
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++ ) {
+      const columnOfFour = [i, i +width, i + width * 2, i + width * 3]
+      const decidedColor = currentColorArrangement[i]
+
+      if ( columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+          columnOfFour.forEach(square => currentColorArrangement[square] = '') 
+      }
+    }
+  }
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++ ) {
+      const columnOfThree = [i, i +width, i + width *2]
+      const decidedColor = currentColorArrangement[i]
+
+      if ( columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+          columnOfThree.forEach(square => currentColorArrangement[square] = '') 
+      }
+    }
+  }
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++ ) {
+      const rowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+      if ( columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+          columnOfThree.forEach(square => currentColorArrangement[square] = '') 
+      }
+    }
+  }
+
+
+
   const createBoard = () => {
     const randomColorArrangement = []
     for (let i = 0; i < width * width; i++) {
@@ -26,6 +61,15 @@ const App = () => {
     createBoard()
   }, [])
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour()
+      checkForColumnOfThree()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 100)
+    return ()=> clearInterval(timer)
+  }, [checkForColumnOfFour, checkForColumnOfThree, currentColorArrangement])
+
   console.log(currentColorArrangement)
 
   return (
@@ -35,6 +79,7 @@ const App = () => {
           <img
             key={index}
             style={{backgroundColor: candyColor}}
+            alt={candyColor}
             />
         ))}
 
